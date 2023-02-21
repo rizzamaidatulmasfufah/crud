da1a6854d5018d4e3c0ca116f809353e4663bb31
rizza maidatul masfufah <rizzamaidatulmasfufah@gmail.com>
2023-02-21 13:53:48 +0700

    template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -59,7 +59,7 @@
             </div> -->
 
             <!-- Nav Item - Pages Collapse Menu -->
-            <!-- <li class="nav-item">
+            <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
@@ -72,10 +72,10 @@
                         <a class="collapse-item" href="cards.html">Cards</a>
                     </div>
                 </div>
-            </li> -->
+            </li>
 
             <!-- Nav Item - Utilities Collapse Menu -->
-            <!-- <li class="nav-item">
+            <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-wrench"></i>
@@ -91,7 +91,7 @@
                         <a class="collapse-item" href="utilities-other.html">Other</a>
                     </div>
                 </div>
-            </li> -->
+            </li>
 
             <!-- Divider -->
             <!-- <hr class="sidebar-divider"> -->
