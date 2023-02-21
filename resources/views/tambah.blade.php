e109bf70b36d2f9142e13c06b82ec511882f3213
niken <nikencahyaning157@gmail.com>
2023-02-21 11:21:37 +0700

    button

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,10 +1,10 @@
 
 <div class="card">
     <div class="card-body">
-      <h1>Form Tambah Data gyuvguvbuBuku</h1>
+      <h1>Form Tambah Data</h1>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" rel="stylesheet">
-                        
+
     <form action="/buku/store" method="post">
         {{ csrf_field() }}
         <div class="mb-3">
